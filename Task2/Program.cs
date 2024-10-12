5b099f7c199964d0c5c250f43ba5e8f123f795ba
mcadamskiy <mcadamskiy@gmail.com>
2024-10-12 16:21:50 +0500

    Task2 is ended

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 0;
+        int[] a = [7, 9, 8, 43, 66, 66, 84, 68, 95, 72];
+        for(int i = 0; i < 10; i ++)
+        {
+            if(a[i] % 2 == 0)
+            {
+                n ++;
+            }
+        }
+        System.Console.WriteLine(n);
+    }
+}
