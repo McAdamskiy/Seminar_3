f6a71f4fb2b9a2962e364bb6c039bfd3b2e06ae0
mcadamskiy <mcadamskiy@gmail.com>
2024-10-12 14:31:57 +0500

    Task1 is ended

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+
+    static void Main()
+    {
+        int[] a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 99];
+        for (int i = 0; i < 10; i ++)
+        {
+            if(a[i] > 20 && a[i] < 90) 
+            {
+                Console.Write(a[i]);
+                Console.Write(", ");
+            }
+        }
+
+    }
+}
