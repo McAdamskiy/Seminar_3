aad9f25af567c35c8fcd8ef76a89bd4a50fffc93
mcadamskiy <mcadamskiy@gmail.com>
2024-10-12 16:49:26 +0500

    Task3 is ended

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] a = [2.3, 4.6, 4.9, 9.8, 5.2, 1.1, 13.6];
+        Array.Sort(a);
+        Console.WriteLine(a[6] - a[0]);
+    }
+}
